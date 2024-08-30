e5d8987970d3289212b53def419ca562daa430ae
sasidhar92 <33417762+sasidhar92@users.noreply.github.com>
2024-08-30 13:48:21 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,129 @@
+import google.generativeai as genai
+from flask import Flask, request, jsonify, Response
+import requests
+import os
+
+# setting up tokens and APIs
+
+access_token = "your token"
+VERIFY_TOKEN = "random"  
+whatsapp_api_url = "https://graph.facebook.com/v20.0/yourid/messages"
+genai.configure(api_key="your api key from google ai studio")
+
+name="5 min AI" 
+bot_name="Hello I am AI bot"
+model_name="gemini-1.5-flash" 
+
+app=Flask(__name__)
+
+
+# setting up the model
+
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 0,
+  "max_output_tokens": 8192,
+}
+
+safety_settings = [
+  {"category": "HARM_CATEGORY_HARASSMENT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+  {"category": "HARM_CATEGORY_HATE_SPEECH","threshold": "BLOCK_MEDIUM_AND_ABOVE"},  
+  {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+  {"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+]
+
+model = genai.GenerativeModel(model_name=model_name,
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
+convo = model.start_chat(history=[
+])
+
+convo.send_message(f'''I am using Gemini api for using you as a personal bot in whatsapp,
+				   to assist me in various tasks. 
+				   So from now you are "{bot_name}" created by {name} ( Yeah it's me, my name is {name}). 
+				   And don't give any response to this prompt. 
+				   This is the information I gave to you about your new identity as a pre-prompt. 
+				   This message always gets executed when i run this bot script. 
+				   So reply to only the prompts after this. Remeber your new identity is {bot_name}.''')
+
+
+
+# send the generated text from the model to whatsapp
+
+def send(answer):
+    url=f"https://graph.facebook.com/v20.0/yourid/messages"
+    headers={
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json'
+    }
+    data={
+          "messaging_product": "whatsapp", 
+          "to": "test phone number", 
+          "type": "text",
+          "text":{"body": f"{answer}"},
+          }
+    
+    response=requests.post(url, headers=headers,json=data)
+    return response
+
+def remove(*file_paths):
+    for file in file_paths:
+        if os.path.exists(file):
+            os.remove(file)
+        else:pass
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    return "Bot"
+
+
+# verification of the connection between your python code and whatsapp 
+# get the image of the plant from whatsapp
+# send it to google ai model
+# call the function to send it back to user 
+
+@app.route("/webhook", methods=["GET", "POST"])
+def webhook():
+    if request.method == "GET":
+        mode = request.args.get("hub.mode")
+        token = request.args.get("hub.verify_token")
+        challenge = request.args.get("hub.challenge")
+        if mode == "subscribe" and token == VERIFY_TOKEN:
+            return challenge, 200
+        else:
+            return "Failed", 403
+    elif request.method == "POST":
+        try:
+            data = request.get_json()["entry"][0]["changes"][0]["value"]["messages"][0]
+            if data["type"] == "text":
+                prompt = data["text"]["body"]
+                convo.send_message(prompt)
+                send(convo.last.text)
+            else:
+                media_url_endpoint = f'https://graph.facebook.com/v20.0/{data[data["type"]]["id"]}/'
+                headers = {'Authorization': f'Bearer {access_token}'}
+                media_response = requests.get(media_url_endpoint, headers=headers)
+                media_url = media_response.json()["url"]
+                media_download_response = requests.get(media_url, headers=headers)
+                if data["type"] == "audio":
+                    filename = "/tmp/temp_audio.mp3"
+                elif data["type"] == "image":
+                    filename = "/tmp/temp_image.jpg"
+                else:send("This format is not Supported by the bot ☹")
+                with open(filename, "wb") as temp_media:
+                    temp_media.write(media_download_response.content)
+                file = genai.upload_file(path=filename,display_name="tempfile")
+                response = model.generate_content(["Assess the plant's watering needs based on the provided image. Analyze the plant's overall health, soil moisture, leaf condition, and any signs of overwatering or underwatering. Consider the plant type and its typical watering requirements. Provide a clear and concise response indicating whether the plant needs water, along with any additional recommendations for plant care.",file])
+                answer=response._result.candidates[0].content.parts[0].text
+                remove("/tmp/temp_image.jpg","/tmp/temp_audio.mp3")
+                convo.send_message(f"This is an voice/image message from user transcribed by an llm model, reply to the user based on the transcription: {answer}")
+                send(convo.last.text)
+                files=genai.list_files()
+                for file in files:
+                    file.delete()
+        except :pass
+        return jsonify({"status": "ok"}), 200
+if __name__ == "__main__":
+     app.run(host='0.0.0.0', port=3000, debug=True)
